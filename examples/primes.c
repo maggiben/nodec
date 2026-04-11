6c45d8fbb004d1399eb157c972ef439297fcc0ee
maggiben <benjaminmaggi@gmail.com>
2026-04-11 03:03:20 -0300

    chore: prime example

---
+++ examples/primes.c
@@ -0,0 +1,39 @@
+#define TRUE 1
+#define FALSE 0
+#include <stdio.h>
+#include <unistd.h>
+
+int is_prime(int n) {
+  int d;
+  if (n < 2) {
+    return FALSE;
+  }
+  if (n == 2) {
+    return TRUE;
+  }
+  if (n % 2 == 0) {
+    return FALSE;
+  }
+  d = 3;
+  while (d <= n / d) {
+    if (n % d == 0) {
+      return FALSE;
+    }
+    d = d + 2;
+  }
+  return TRUE;
+}
+
+int main() {
+  int n;
+  printf("Enter the initial number: ");
+  scanf("%d", &n);
+  while (TRUE) {
+    if (is_prime(n)) {
+      printf("%d\n", n);
+      sleep(1);
+    }
+    n = n + 1;
+  }
+  return 0;
+}
